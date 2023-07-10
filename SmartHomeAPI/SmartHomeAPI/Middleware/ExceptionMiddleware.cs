83dc9984aaee5af5ed7bc6cac408705b8beec44a
Marieke de Zwart <mzwart@rademaker.nl>
2023-07-10 17:11:56 +0200

    Added extension method for middleware

--- SmartHomeAPI/SmartHomeAPI/Middleware/ExceptionMiddleware.cs
+++ SmartHomeAPI/SmartHomeAPI/Middleware/ExceptionMiddleware.cs
@@ -6,10 +6,10 @@ namespace SmartHomeAPI.Middleware;
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
+    private readonly ILogger<ExceptionMiddleware> _logger;
 
-    public ExceptionMiddleware(RequestDelegate next, ILogger logger)
-    {
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {   
         _next = next;
         _logger = logger;
     }
---
+++ SmartHomeAPI/SmartHomeAPI/Middleware/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SmartHomeAPI.Middleware;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static void UseExceptionMiddleware(this WebApplication app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+    }
+}
