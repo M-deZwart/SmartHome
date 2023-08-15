9a85522ffa3ce507540a3efe7cb39fbff36480b6
MdeZwart <mariekedezwart@outlook.com>
2023-08-15 15:07:58 +0200

    changed must to lessthanorequalto

--- SmartHomeAPI/Domain/Domain/Validators/HumidityValidator.cs
+++ SmartHomeAPI/Domain/Domain/Validators/HumidityValidator.cs
@@ -9,7 +9,7 @@ public class HumidityValidator : AbstractValidator<Humidity>
     {
         RuleFor(humidity => humidity.Percentage).InclusiveBetween(1, 100).WithMessage("Invalid percentage value. The humidity percentage should be between 0 and 100.");
         RuleFor(humidity => humidity.Date)
-            .Must((date) => date <= DateTime.Now)
+            .LessThanOrEqualTo(date => DateTime.Now)
             .WithMessage("Date cannot be in the future.");
     }
 }
